bd843b22ad0acfb0be318552d3b4ed27bd809d5e
Łukasz <l.hajduk@dnavi.pl>
2026-06-09 20:20:22 +0200

    fix: add SoftDeletes trait in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,11 +11,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[UseFactory(TaskFactory::class)]
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
